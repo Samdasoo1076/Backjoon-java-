5c2e609e968d7539a877b22cd874562b64e96d8a
이지민 <dlwlalsdkd@gmail.com>
2024-05-10 10:33:50 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int score = sc.nextInt();
+        String test = "";
+        
+        if(score >= 90) {
+            test = "A";
+        } else if(score >= 80 && score < 90) {
+            test = "B";
+        } else if(score >= 70 && score < 80) {
+            test = "C";
+        } else if(score >= 60 && score < 70) {
+            test = "D";
+        } else {
+            test = "F";
+        }
+        System.out.println(test);
+    }
+}
