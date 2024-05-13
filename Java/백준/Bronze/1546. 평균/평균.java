1e16575659ffa32d9545838b376e12703314e993
이지민 <dlwlalsdkd@gmail.com>
2024-05-13 10:55:23 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18640 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++) {
+        	arr[i] = sc.nextInt();        
+        	}
+        
+        for(int i = 0; i < arr.length; i++) {
+        	if(arr[i] > max ) {
+        		max = arr[i];
+        	}
+        }
+        
+        double[] scores = new double[N];
+        for(int i = 0; i < N; i++) {
+        	scores[i] = (double) arr[i] / max * 100;
+        }
+        
+        double sum = 0;
+        for(int i = 0; i < N; i++) {
+        	sum += scores[i];
+        }
+        double total = sum / N;
+        System.out.print(total);
+    }
+}
+ 
