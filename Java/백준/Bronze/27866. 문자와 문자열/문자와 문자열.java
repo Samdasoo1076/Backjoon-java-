84bac1953c0d8223d93ca14c922e2b5bf7f8a567
이지민 <dlwlalsdkd@gmail.com>
2024-05-22 11:51:57 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    	 Scanner sc = new Scanner(System.in);
+    	 String tx = sc.nextLine();
+    	 int n = sc.nextInt();
+    	
+    	 System.out.println(tx.charAt(n - 1));
+    	 
+        }		
+    }
